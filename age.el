b14fb8c4db83d49bc42910e66eb4441035769ff8
Bas Alberts <13686387+anticomputer@users.noreply.github.com>
2025-02-27 22:58:08 -0500

    Update age.el version to v0.1.7

--- age.el
+++ age.el
@@ -9,7 +9,7 @@
 ;; Homepage: https://github.com/anticomputer/age.el
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: data
-;; Version: 0.1.6
+;; Version: 0.1.7
 
 ;; This file is NOT part of GNU Emacs.
 
